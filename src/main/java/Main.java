0941448c4cc2b294d0974b5c06c6550bf040d142
Kadir <kadirgucuyener35@gmail.com>
2022-12-12 20:32:31 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            boolean isAsal = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isAsal = false;
+                    break;
+                }
+            }
+            if (isAsal)
+                System.out.print(i + ", ");
+        }
+    }
+}
